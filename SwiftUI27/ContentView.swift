d43ebfe89ee3f209482fe82f8530d03e340eb4b5
Rohit Saini <sketchlearn7@gmail.com>
2020-08-07 10:00:36 +0530

    code Updated

--- SwiftUI27/ContentView.swift
+++ SwiftUI27/ContentView.swift
@@ -10,12 +10,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ScrollView(.vertical,showsIndicators: false){
+            VStack{
+                GeometryReader{ geo  in
+                    Image("img").resizable()
+                        .offset(y: geo.frame(in: .global).minY > 0 ? -geo.frame(in: .global).minY : 0)
+                        .frame(height:geo.frame(in: .global).minY > 0 ? geo.frame(in: .global).minY + 300 : 300)
+                       Spacer()
+                }.frame(height: 300)
+             
+            }
+            ForEach(0..<18){ _ in
+                HStack{
+                    Image("img").resizable().frame(width:50,height:50).clipShape(Circle())
+                    VStack{
+                        Text("Rohit Saini").fontWeight(.heavy)
+                        Text("Singer").fontWeight(.regular)
+                    }
+                    Spacer()
+                }.padding()
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
